d10e0a942794f83a5b85a56f697e903f5c5a78a3
TaeGN <110441723+TaeGN@users.noreply.github.com>
2022-08-17 12:13:33 +0900

    20220817

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,25 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args)  {
+		System.out.println("Hello World!");
+		System.out.println("java");
+		// 주석 : 프로그램 실행에 영향 x
+		/*
+		  여러줄 주석
+		  
+		  */
+		
+		// 단축키 ctrl + /
+		
+//		System.out.println();
+		
+		// 힌트 얻기 : ctrl + space
+		// 한 줄 지우기 : ctrl + d
+		// 코드 위아래로 옮기기 : art + ↕
+		// 콘솔 출력 코드 : sysout / sout
+		
+		System.out.println("");
+		
+	}
+}
